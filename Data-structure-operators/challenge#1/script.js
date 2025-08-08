328be373f996a2c33bfe1f231a6eb08d1e8139e0
Tayyab khan <tk7635373@gmail.com>
2025-08-08 06:44:36 +0500

    Added Data-structure-operators challenge#1

---
+++ Data-structure-operators/challenge#1/script.js
@@ -0,0 +1,103 @@
+const game = {
+  team1: "Bayern Munich",
+  team2: "Borrussia Dortmund",
+  players: [
+    [
+      "Neuer",
+      "Pavard",
+      "Martinez",
+      "Alaba",
+      "Davies",
+      "Kimmich",
+      "Goretzka",
+      "Coman",
+      "Muller",
+      "Gnarby",
+      "Lewandowski",
+    ],
+    [
+      "Burki",
+      "Schulz",
+      "Hummels",
+      "Akanji",
+      "Hakimi",
+      "Weigl",
+      "Witsel",
+      "Hazard",
+      "Brandt",
+      "Sancho",
+      "Gotze",
+    ],
+  ],
+  score: "4:0",
+  scored: ["Lewandowski", "Gnarby", "Lewandowski", "Hummels"],
+  date: "Nov 9th, 2037",
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+
+const players1 = [
+  "Neuer",
+  "Pavard",
+  "Martinez",
+  "Alaba",
+  "Davies",
+  "Kimmich",
+  "Goretzka",
+  "Coman",
+  "Muller",
+  "Gnarby",
+  "Lewandowski",
+];
+const players2 = [
+  "Burki",
+  "Schulz",
+  "Hummels",
+  "Akanji",
+  "Hakimi",
+  "Weigl",
+  "Witsel",
+  "Hazard",
+  "Brandt",
+  "Sancho",
+  "Gotze",
+];
+
+const gk = ["Neuer"];
+const fieldPlayers = [
+  "Schulz",
+  "Hummels",
+  "Akanji",
+  "Hakimi",
+  "Weigl",
+  "Witsel",
+  "Hazard",
+  "Brandt",
+  "Sancho",
+  "Gotze",
+];
+
+const allPlayers = [...players1, ...players2];
+// console.log(allPlayers);
+
+const players1Final = [...players1, "Thiago", "Coutinho", "Perisic"];
+console.log(players1Final);
+
+const { team1, x: draw, team2 } = game.odds;
+
+function printGoals(...players) {
+  for (let player of players) {
+    console.log(player);
+  }
+  console.log(`${players.length} goal were scored`);
+}
+
+printGoals("Davies", "Muller", "Lewandowski", "kimmich");
+
+game.odds.team1 < game.odds.team2 &&
+  console.log(`${game.team1} is more likely to win`);
+game.odds.team2 > game.odds.team1 &&
+  console.log(`${game.team2} is more likely to win`);
