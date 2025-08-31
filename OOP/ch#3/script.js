83ff68d509a5c4e7c209f59db361df20840e5629
Tayyab khan <tk7635373@gmail.com>
2025-08-31 16:24:00 +0500

    Added ch#3 folder inside the OOP folder

---
+++ OOP/ch#3/script.js
@@ -0,0 +1,36 @@
+function Car(make, speed) {
+  this.make = make;
+  this.speed = speed;
+}
+
+function EV(make, speed, charge) {
+  Car.call(this, make, speed);
+  this.charge = charge;
+}
+
+EV.prototype = Object.create(Car.prototype);
+EV.prototype.constructor = EV;
+
+EV.prototype.chargeBattery = function (chargeTo) {
+  this.charge = chargeTo;
+};
+
+EV.prototype.accelarate = function () {
+  this.speed += 20;
+  this.charge--;
+  console.log(
+    `${this.make} going at ${this.speed} km/h, with a charge of ${this.charge}% `
+  );
+};
+
+EV.prototype.brake= function(){
+    this.speed -=5;
+    console.log(`${this.make} going at ${this.speed} km/h`)
+}
+
+
+
+const tesla= new EV('Tesla',120,23);
+tesla.accelarate();
+tesla.brake();
+tesla.chargeBattery(90);
